46f89d3658a2f46ef2bfaf41d9a1e8eae2b1e90b
Al-Farhan-Seg <farhan.segujja@gmail.com>
2025-10-19 22:34:49 +0300

    Added an Ordinary and Premium member class inheriting from Member

--- member_module.py
+++ member_module.py
@@ -29,3 +29,35 @@ class Member:
     def get_email(self):
         return self.__email
     
+class Premium_Member(Member):
+    def __init__(self, member_ID, first_name, last_name, gender, contact, address, position):
+        super().__init__(member_ID, first_name, last_name, gender, contact, address)
+        self.loan_svg_percentage = 50
+        self.position = position
+
+    def terminate_Member(self, person):
+        print(f"The {self.position} ({self.first_name} {self.last_name}) has terminated {person} from the SACCO.")
+
+    def make_policy(self, policy):
+        print(f"The {self.position} (Mr./Mrs. {self.first_name}) has approved new policy: {policy}.")
+
+
+class Ordinary_Member(Member):
+    def __init__(self, member_ID, first_name, last_name, gender, contact, address, voting):
+        super().__init__(member_ID, first_name, last_name, gender, contact, address)
+        self.loan_svg_percentage = 10
+        self.voting = True
+
+    def vote_on_policy(self, decision):
+        if self.voting == True:
+            print(f"Name: {self.first_name} {self.last_name}")
+            print("Voting Rights: ACCEPTED")
+            print(f"Decision: {decision}")
+        else:
+            print(f"Name: {self.first_name} {self.last_name}")
+            print("Voting Rights: NOT ALLOWED")
+            print(f"Decision: INVALID")
+
+
+
+
