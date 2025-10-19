98cb89e446fb0f8b114e0ca6e06c667be699a8fa
Al-Farhan-Seg <farhan.segujja@gmail.com>
2025-10-19 22:37:01 +0300

    Added a Fulltime and PartTime class inheriting from the STAFF class, doing some polymorphism

--- staff_module.py
+++ staff_module.py
@@ -1,12 +1,12 @@
 class Staff:
 
-    def __init__(self, staff_ID, first_name, last_name, gender, position, member_ID):
+    def __init__(self, staff_ID, first_name, last_name, gender, position):
         self.staff_ID = staff_ID
         self.first_name = first_name
         self.last_name = last_name
         self.gender = gender
         self.position = position
-        self.member_ID = member_ID
+        self.salary = 0
 
 
     def Approve_loan(self):
@@ -20,3 +20,26 @@ class Staff:
     def Generate_reports(self):
         print(f"Report generated.......\nGenereted by: {self.first_name} {self.last_name}")
         # return f"Report generated.......\nGenereted by: {self.first_name} {self.last_name}"
+
+    def compute_Salary(self):
+        return self.salary
+
+
+# Polymorphism on its way here.........
+class Fulltime_Staff(Staff):
+    def __init__(self, staff_ID, first_name, last_name, gender, position, basic_salary):
+        super().__init__(staff_ID, first_name, last_name, gender, position)
+        self.basic_salary = basic_salary
+
+    def compute_Salary(self):
+        return self.basic_salary
+    
+
+class PartTime_Staff(Staff):
+    def __init__(self, staff_ID, first_name, last_name, gender, position, rate_per_hour):
+        super().__init__(staff_ID, first_name, last_name, gender, position,)
+        self.rate_per_hour = rate_per_hour
+
+    def compute_Salary(self, hours_worked):
+        return hours_worked * self.rate_per_hour
+    
