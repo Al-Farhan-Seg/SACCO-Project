02bba0f91cd3e65c36765e803c78fd5eccf13b59
Al-Farhan-Seg <farhan.segujja@gmail.com>
2025-11-12 05:00:36 +0300

    nothing much

--- loan_module.py
+++ loan_module.py
@@ -40,19 +40,13 @@ class Loan_Payment(Loan):
         self.loan_amount = self.loan_amount - self.amount
         print("LOAN PAYMENT RECEIVED".center(50, "."))
 
-        print(f""" ------------------------------------------------
+        return f""" ------------------------------------------------
 |             Payment ID: {self.payment_ID}                 |
 | Amount received: UGX.{self.amount} from '{self.member_ID}'        |
 | for '{self.month}-{self.year}' on Date: {self.date}        |
 | Outstanding Loan Amount: UGX.{self.loan_amount}          |
- ------------------------------------------------""")
+ ------------------------------------------------"""
         
 
-'''l1 = Loan("L001", "M001", 20000000, 23)
-l1.set_payment_amount()
-l1.get_payment_amount()
-l1.Approve_Loan()
 
-l1_p = Loan_Payment(l1.loan_ID, l1.member_ID, l1.loan_amount, l1.payment_period, "PiD001", 120000, "February", "2025", "10-19-2025")
-l1_p.Record_Payment()'''
     
