50e06dc8d8912dbf9406514bba0d1324ca376c17
Al-Farhan-Seg <farhan.segujja@gmail.com>
2025-11-14 21:12:06 +0300

    added Polymorphic logic😁

--- staff_module.py
+++ staff_module.py
@@ -1,4 +1,3 @@
-from member_module import *
 class Staff:
 
     def __init__(self, staff_ID, first_name, last_name, gender, position):
@@ -7,7 +6,7 @@ class Staff:
         self.last_name = last_name
         self.gender = gender
         self.position = position
-        self.salary = 0
+        self.__basic_salary = 0
 
 
     def Approve_loan(self):
@@ -23,27 +22,35 @@ class Staff:
         return f"Report generated.......\nGenereted by: {self.first_name} {self.last_name}"
 
     def compute_Salary(self):
-        return self.salary
+        return self.__basic_salary
 
 
 # Polymorphism on its way here.........
 class Fulltime_Staff(Staff):
-    def __init__(self, staff_ID, first_name, last_name, gender, position, basic_salary):
+    def __init__(self, staff_ID, first_name, last_name, gender, position, experience):
         super().__init__(staff_ID, first_name, last_name, gender, position)
-        self.basic_salary = basic_salary
+        self.__basic_salary = 0
+        self.experience = experience
+
+    def set_salary(self, amount):
+        self.__basic_salary = amount
+        return f"Salary for {self.first_name} {self.last_name} successfully set"
 
     def compute_Salary(self):
-        return self.basic_salary
+        if self.experience > 7:
+            bonus = 0.4 * self.__basic_salary
+        else:
+            bonus = 0.1 * self.__basic_salary
+        self.__basic_salary += bonus
+
+        return self.__basic_salary
     
 
 class PartTime_Staff(Staff):
     def __init__(self, staff_ID, first_name, last_name, gender, position, rate_per_hour):
-        super().__init__(staff_ID, first_name, last_name, gender, position,)
+        super().__init__(staff_ID, first_name, last_name, gender, position)
         self.rate_per_hour = rate_per_hour
 
     def compute_Salary(self, hours_worked):
         return hours_worked * self.rate_per_hour
-    
-
-
     
