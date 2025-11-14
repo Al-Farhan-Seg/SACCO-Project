dbf6ec2ed09ae4fab271f37e45fea17896c7c7ad
Al-Farhan-Seg <farhan.segujja@gmail.com>
2025-11-14 21:10:11 +0300

    removed 'date' attribute and added automatic-time

--- saving_module.py
+++ saving_module.py
@@ -1,6 +1,5 @@
-from account_module import *
+from datetime import datetime
 class Saving:
-
     IDs = []
 
     def ID_gen(self):
@@ -14,13 +13,12 @@ class Saving:
             else:
                 continue
 
-    def __init__(self, amount, month, year, date): #removed ACCOUNT_NO and auto-generated DEPOSIT_ID
+    def __init__(self, amount, for_month, for_year): #removed 'ACCOUNT_NO' and auto-generated DEPOSIT_ID and also removed 'date'
         self.deposit_ID = self.ID_gen()
-        #self.account_No = account_No
         self.amount = amount
-        self.month = month
-        self.year = year
-        self.date = date
+        self.for_month = for_month
+        self.for_year = for_year
+        self.date = datetime.now().strftime("%d-%m-%Y")
 
     # Composition application............
     def __Update_Balance(self, account_obj):
@@ -35,16 +33,4 @@ Deposit ID: {self.deposit_ID}
 Acc_No: {account_obj.account_No[0:4]}-XXXX
 Amount: {self.amount}
 Date of payment: {self.date}
-for: {self.month}-{self.year}"""
-
-
-
-
-
-#------TESTING--------------
-s = Saving(20000, "February", 2020, "Date")
-print(s.deposit_ID)
-
-acc1 = Account("4070-4300", "MB-001", "Ordinary Savings")
-print(s.Record_Deposit(acc1))
-print(acc1._Account__balance)
+for: {self.for_month}-{self.for_year}"""
