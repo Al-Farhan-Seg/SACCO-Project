596cc14da83a1f0ac70ff53ccd2beeeb5447096e
Al-Farhan-Seg <farhan.segujja@gmail.com>
2025-11-14 21:11:17 +0300

    added withdrawal logic plus automatic-time

--- withdraw_module.py
+++ withdraw_module.py
@@ -1,21 +1,32 @@
+from time import *
+from datetime import datetime
 class Withdraw:
 
-    def __init__(self, withdraw_ID, account_No, amount, date):
+    def __init__(self, withdraw_ID, account):
+
         self.withdraw_ID = withdraw_ID
-        self.account_No = account_No
-        self.amount = amount
-        self.date = date
+        self.account = account
+        self.date = datetime.now().strftime("%A/%d/%m/%Y, %H:%M:%S")
         
 
-    def Check_Balance(self):
-        print(f"Checking balance of Acc_No. {self.account_No}.......")
+    def Initiate_Withdraw(self): #renamed from Check_Balance(self)
+        print(f"Checking balance of Acc_No. {self.account.account_No}.......")
+        sleep(2)
+        query = input(f"""You account has {self.account.Get_Balance()} remaining,
+Are you sure you want to leave the SACCO and empty your {self.account.account_type} account? (Yes/No)""")
+
+        if query[0].upper() == "Y":
+            self.__Update_Balance()
+            sleep(2)
+            self.__Return_Result()
+        else:
+            print("The SACCO is happy to have you around🥳🥳🥳")
         
 
-    def Update_Balance(self):
-        # find a way of trying to make the account balance ZERO(0)
-        print(f"Balance of Acc.No. {self.account_No} has been updated........")
+    def __Update_Balance(self):
+        # making the account balance ZERO(0)
+        self.account._Account__balance = 0
+        print(f"Balance of Acc.No. {self.account.account_No} has been SUCCESSFULLY updated to {self.account._Account__balance}")
     
-    def Return_Result(self):
-        print(f"Owner of Acc_No {self.account_No} has succesfully withdrawn from the SACCO......")
-
-    
+    def __Return_Result(self):
+        print(f"{self.account.member.first_name} {self.account.member.last_name} of Acc_No {self.account.account_No} has succesfully withdrawn from the SACCO on '{self.date}' 😭😭😭")
